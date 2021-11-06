4fa3650658616c25f5fcd885932603886d3a1b02
Peter Sovietov <peter.sovietov@gmail.com>
2021-11-06 23:20:13 +0300

    more powerful and yet smaller fortik!

--- fortik.py
+++ fortik.py
@@ -1,63 +1,37 @@
-﻿# Author: Peter Sovietov
-
-import sys
+﻿import sys
 
 
-def compile_colon(words, tokens):
-    name, defined = tokens.pop(0), []
-    while tokens[0] != ';':
-        defined.append(tokens.pop(0))
-    tokens.pop(0)
-    words[name] = parse(words, defined)
-
-
-def parse(words, tokens):
-    code = []
-    while tokens:
-        word = tokens.pop(0)
-        if word.isdigit():
-            code.append(('int', int(word)))
-        elif word == ':':
-            compile_colon(words, tokens)
-        elif word == 'repeat':
-            code += [('name', tokens.pop(0)), ('call', 'repeat')]
-        elif word == 'ifelse':
-            code += [('name', tokens.pop(0)),
-                     ('name', tokens.pop(0)), ('call', 'ifelse')]
+def parse(words, tokens, pos=0):
+    ast = [[]]
+    while pos < len(tokens):
+        if tokens[pos] == '[':
+            ast.append([])
+        elif tokens[pos] == ']':
+            quote = ast.pop()
+            ast[-1].append(('push', quote))
+        elif tokens[pos] == 'is':
+            pos += 1
+            name = tokens[pos]
+            words[name] = ast[-1].pop()[1]
+        elif tokens[pos].isdigit():
+            ast[-1].append(('push', int(tokens[pos])))
         else:
-            code.append(('call', word))
-    return code
+            ast[-1].append(('call', tokens[pos]))
+        pos += 1
+    return ast[0]
 
 
-def execute(words, stack, code):
-    for t, v in code:
-        if t in ('int', 'name'):
-            stack.append(v)
-        elif t == 'call':
-            if v in words:
-                execute(words, stack, words[v])
-            elif v in PRIMS:
-                PRIMS[v](words, stack)
+def execute(words, stack, ast):
+    for tag, val in ast:
+        if tag == 'push':
+            stack.append(val)
+        elif tag == 'call':
+            if val in words:
+                execute(words, stack, words[val])
+            elif val in PRIMS:
+                PRIMS[val](words, stack)
             else:
-                sys.exit('unknown word: ' + v)
-
-
-def binop(func):
-    def word(words, stack):
-        tos = stack.pop()
-        stack.append(func(stack.pop(), tos))
-    return word
-
-
-def repeat(words, stack):
-    name = stack.pop()
-    for _ in range(stack.pop()):
-        execute(words, stack, words[name])
-
-
-def ifelse(words, stack):
-    name2, name1 = stack.pop(), stack.pop()
-    execute(words, stack, words[name1 if stack.pop() else name2])
+                sys.exit('unknown word: ' + val)
 
 
 def dup(words, stack):
@@ -76,18 +50,29 @@ def emit(words, stack):
     print(chr(stack.pop()), end='')
 
 
+def ifelse(words, stack):
+    f_ast, t_ast = stack.pop(), stack.pop()
+    execute(words, stack, t_ast if stack.pop() else f_ast)
+
+
+def binop(func):
+    def word(words, stack):
+        tos = stack.pop()
+        stack.append(func(stack.pop(), tos))
+    return word
+
+
 PRIMS = {
     '+': binop(lambda a, b: a + b),
     '-': binop(lambda a, b: a - b),
     '*': binop(lambda a, b: a * b),
     '/': binop(lambda a, b: a // b),
     '<': binop(lambda a, b: int(a < b)),
-    'repeat': repeat,
-    'ifelse': ifelse,
     'dup': dup,
     'drop': drop,
     '.': dot,
-    'emit': emit
+    'emit': emit,
+    'ifelse': ifelse
 }
 
 
@@ -97,15 +82,11 @@ def repl(words, stack):
 
 
 source = '''
-: cr 10 emit ;
-: star 42 emit ;
-: star-line dup repeat star cr ;
-: star-rect repeat star-line drop ;
-4 8 star-rect
-: fact1 drop 1 ;
-: fact2 dup 1 - fact * ;
-: fact dup 1 < ifelse fact1 fact2 ;
-5 fact .'''
+[
+  dup 1 < [ drop 1 ] [ dup 1 - fact * ] ifelse
+] is fact
+5 fact .
+'''
 
 words, stack = {}, []
 execute(words, stack, parse(words, source.split()))
